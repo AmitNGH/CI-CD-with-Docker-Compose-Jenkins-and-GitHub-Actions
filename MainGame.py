3eb35f28f9a62e1726b9bb6e5758183fede295cf
AmitNGH <Amit963n@gmail.com>
2024-07-08 20:12:01 +0300

    Changed project struct

--- MainGame.py
+++ MainGame.py
@@ -1,4 +1,7 @@
 from Live import load_game, welcome
+from MainScores import app
+
+app.run(debug=True)
 
 print(welcome("Amit"))
 load_game()
--- MainScores.py
+++ MainScores.py
@@ -1,10 +1,16 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+
+@app.route('/')
 def score_server():
-    body = ('<html>'
-            '   <head>'
-            '           <title>Scores Game</title>'
-            '   </head>'
-            '   <body>'
-            '           <h1>The score is <div id="score">{SCORE}</div></h1>'
-            '   G</body>'
-            '</html>')
+    body_template = ('<html>'
+                     '   <head>'
+                     '           <title>Scores Game</title>'
+                     '   </head>'
+                     '   <body>'
+                     '           <h1>The score is <div id="score">{SCORE}</div></h1>'
+                     '   G</body>'
+                     '</html>')
+    return body_template
